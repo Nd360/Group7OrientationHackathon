68a8a897127867d5ea7a427c43f74412172d102c
mkolonay <mkolonay@yesvirginia.org>
2020-07-10 15:51:23 -0400

    Tweaking the Legend
    Moved to middle..added color ramp...etcetera...

--- Choropleth.R
+++ Choropleth.R
@@ -12,11 +12,25 @@ library(tidyverse)
 
 setwd("C:/Users/micha/Google Drive/GitHub/Group7OrientationHackathon")
 
-
 # Get the data from 
 data <- read_csv("data/owid-covid-data.csv", col_names = T)
-current_data_df  <- data %>% filter(date == '2020-07-10') 
+current_data_df  <- data %>% filter(date == '2020-05-08') 
 # Load it as a geospatial object in R
 my_spdf <- readOGR( dsn= "./data/spatial" , layer="TM_WORLD_BORDERS_SIMPL-0.3", verbose=FALSE) 
-cartography::choroLayer(spdf = my_spdf, spdfid="ISO3" , dfid="iso_code", df=current_data_df, var = "total_cases_per_million", )
-title("Some title")
+
+colors <- c("papayawhip", "pink", "pink1", "hotpink","maroon1", "maroon2", "maroon3", "maroon4")
+
+cartography::choroLayer(spdf = my_spdf
+                        , spdfid="ISO3" 
+                        , dfid="iso_code"
+                        , df=current_data_df
+                        , var = "total_cases_per_million"
+                        , legend.pos = "bottom"
+                        , breaks=c(0, 5, 10, 50, 100, 500, 1000, 2000, Inf) 
+                        , legend.horiz = TRUE
+                        , col = colors
+                        , legend.title.txt = ""
+                        , colNA = "white"
+                        , legend.nodata = "No Data"
+                        )
+title("Total confirmed COVID-19 cases per million people, June 10, 2020")
